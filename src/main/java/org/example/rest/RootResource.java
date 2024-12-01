418e88f186164316dc24f19e0668a08f0c45e62b
yaroslav <yaroslav.chernysh@hpe.com>
2024-12-02 00:36:52 +0200

    Get username from OIDC

--- src/main/java/org/example/rest/RootResource.java
+++ src/main/java/org/example/rest/RootResource.java
@@ -5,6 +5,10 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import org.example.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
 @Path("/")
 public class RootResource {
@@ -13,6 +17,8 @@ public class RootResource {
     @GET
     @Produces("text/plain")
     public String index() {
-        return "Hello, " + person.getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = ((OidcUser)authentication.getPrincipal()).getName();
+        return "Hello, " + userName + "! I know abut " + person.getName() + ", btw...";
     }
 }
